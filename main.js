3ad8aebeacf1bf29bf6570a0ffc55f840cff211d
love-nature <124216212+love-nature@users.noreply.github.com>
2023-07-18 19:42:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+noseX=0;
+noseY=0;
+function preload(){
+
+
+}
+function setup(){
+canvas= createCanvas(300,300);
+canvas.center();
+
+}
+
+
+function draw(){
+
+}
+function take_snapshot(){
+  
+}
+
